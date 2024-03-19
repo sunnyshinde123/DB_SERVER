cb98ba7e0a03dd68ee4c753800c6a7c4dac43c40
sunnyshinde123 <sunnyshinde157@gmail.com>
2024-03-19 17:51:21 +0530

    index.js file updated with patch, delete & post method

--- index.js
+++ index.js
@@ -1,12 +1,32 @@
+/*const { faker } = require('@faker-js/faker');
+
+const express=require("express");
+const app=express();
+let path=require("path");
+methodOverride=require("method-override");
+
+app.use(methodOverride("_method"));
+
+app.set("view engine", "ejs");
+app.set("views", path.join(__dirname,"/views"));
+
+const mysql=require('mysql2');
+const { connect } = require('http2');*/
+
 const { faker } = require('@faker-js/faker');
 
 const express=require("express");
 const app=express();
 let path=require("path");
+const methodOverride=require("method-override");
 app.set("view engine", "ejs");
 app.set("views", path.join(__dirname,"/views"));
 
 const mysql=require('mysql2');
+const { v4: uuidv4 } = require('uuid');
+app.use(methodOverride("_method"));
+app.use(express.urlencoded({extended:true}));
+
 const { connect } = require('http2');
 
 const connection = mysql.createConnection({
@@ -15,6 +35,7 @@ const connection = mysql.createConnection({
     database : "delta_app",
     password : "Sunny@2001",
 });
+
 
 
 let getRandomUser=()=>{
@@ -45,14 +66,9 @@ app.get("/",(req, res)=>{
         res.send("Error in DB");
     }
 })
-
-app.get("/user/search", (req, res)=>{
-    res.render("user.ejs");
-})
-
-app.get("/user/:username", (req, res)=>{
-    let {username}=req.params;
-    let q=`select * from user where username=${username}`;
+app.post("/user/search", (req, res)=>{
+    let {username}=req.body;
+    let q=`select * from user where username='${username}'`;
     try{
         connection.query(q, (err, result, fields)=>{
             if(err){
@@ -60,6 +76,98 @@ app.get("/user/:username", (req, res)=>{
             };
             console.log(result);
             res.render("showuser.ejs", {result});
+        })
+    }catch(err){
+        res.send("Error in DB");
+    }
+})
+
+app.get("/user/search", (req, res)=>{
+    res.render("user.ejs");
+})
+
+app.get("/user/:id/edit",(req, res)=>{
+    let {id}=req.params;
+    console.log(id);
+    let q=`select * from user where id='${id}'`;
+    try{
+        connection.query(q, (err, result, fields)=>{
+            if(err) throw err;
+            let userData=result[0];
+            res.render("edit.ejs",{userData});
+        })
+    }catch(err){
+        res.send("err in db");
+    }
+})
+
+app.patch("/user/:id",(req, res)=>{
+    let {id}=req.params;
+    let {username : newUserName, password : formPass}=req.body;
+    let q=`select * from user where id='${id}'`;
+    try{
+        connection.query(q, (err, result, fields)=>{
+            if(err) throw err;
+            let userData=result[0];
+            console.log(userData);
+            if(formPass!=userData.password){
+                res.send("Wrong Password");
+            }else{
+                let q2=`update user set username='${newUserName}' where id='${id}'`;
+                connection.query(q2, (err, result)=>{
+                    if(err) throw err;
+                    res.redirect("/user");
+                })
+            }
+        })
+    }catch(err){
+        res.send("err in db");
+    }
+})
+
+app.get("/user/new", (req, res)=>{
+    res.render("newEntry.ejs");
+})
+
+app.post("/user", (req, res)=>{
+    let id=uuidv4();
+    let{username : user, email : mail, password : pass}=req.body;
+    let q=`insert into user(id, username, email, password) values ('${id}', '${user}', '${mail}', '${pass}')`;
+    try{
+        connection.query(q, (err, result, fields)=>{
+            if(err) throw err;
+            console.log(result);
+            res.redirect("/user");
+        })
+    }catch(err){
+        res.send("Error in DB");
+    }
+})
+
+app.get("/user/:id/delete", (req, res)=>{
+    let{id}=req.params;
+    res.render("deletePage.ejs",{id});
+})
+
+app.delete("/user/:id",(req, res)=>{
+    let {id}=req.params;
+    let {username : user, password : pass}=req.body;
+    let q=`select * from user where id='${id}'`;
+    try{
+        connection.query(q, (err, result, fields)=>{
+            if(err) throw err;
+            let userData=result[0];
+            if(userData.username==user && userData.password==pass){
+                let q2=`delete from user where id='${userData.id}'`;
+                connection.query(q2, (err, result, fields)=>{
+                    if(err) throw err;
+                    console.log(result);
+                    res.redirect("/user");
+                })
+            }
+            else{
+                res.send("Wrong Username & Password");
+            }
         })
     }catch(err){
         res.send("Error in DB");
@@ -77,6 +185,22 @@ app.get("/user", (req, res)=>{
         res.send("Error in DB");
     }
 })
+
+// app.get("/user/:username", (req, res)=>{
+//     let {username}=req.params;
+//     let q=`select * from user where username=${username}`;
+//     try{
+//         connection.query(q, (err, result, fields)=>{
+//             if(err){
+//                 throw err;
+//             };
+//             console.log(result);
+//             res.render("showuser.ejs", {result});
+//         })
+//     }catch(err){
+//         res.send("Error in DB");
+//     }
+// })
 
 
 // let q="insert into user(id, username, email, password) values ?";
